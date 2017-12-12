e17661b25369bac1c2e1782399491fe2b2deb336
genggjs <11697070@qq.com>
2017-12-12 15:02:06 +0800

    first

---
+++ src/main/java/com/study/server/StudyServerApplication.java
@@ -0,0 +1,14 @@
+package com.study.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class StudyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyServerApplication.class, args);
+	}
+}
